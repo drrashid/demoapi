bc06c4bc52150ad62f1e0b9f263e567ef2f3cf6e
drrashid <drrashidstat@gmail.com>
2019-10-10 21:56:12 +0600

    JSON Response creation

--- api.php
+++ api.php
@@ -1,3 +1,21 @@
 <?php
-    echo "Learn Hunter";
+    header('content-type: application/json');
+    $request = $_SERVER['REQUEST_METHOD'];
+    switch ($request) {
+        case 'GET':
+            echo '{"name": "GET....Dr. Rashid"}';
+            break;
+        case 'POST':
+            echo '{"name": "POST....Dr. Rashid"}';
+            break;
+        case 'PUT':
+            echo '{"name": "PUT....Dr. Rashid"}';
+            break;
+        case 'DELETE':
+            echo '{"name": "DELETE....Dr. Rashid"}';
+            break;
+        default:
+            echo '{"name": "Default"}';
+            break;
+    }
 ?>
